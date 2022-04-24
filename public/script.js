14254c5e8a9d72282d5030f84144eb1faa86c13d
tomray22 <raythomascruz@gmail.com>
2022-04-24 00:00:55 -0700

    testing json movement
    ajax might work

--- public/script.js
+++ public/script.js
@@ -1,17 +1,53 @@
 function handleFormSubmit(event) {
-  event.preventDefault();
 
-  const data = new FormData(event.target);
+$.ajax({
+type:"POST",
+url:"/input",
+data:{'addresses':$('#addr').val(), 'cites':$('#city').val(), 'zips':$('#zip').val(), 'latitudes':$('#lat').val(), 'longitudes':$('#long').val(), 'states':$('#state').val()},
+contentType: "application/json",
+dataType: 'json',
+success:function(data){
 
-  const formJSON = Object.fromEntries(data.entries());
+alert("went through ajax")
 
-  // for multi-selects, we need special handling
-  //formJSON.multi = data.getAll('multi');
+event.preventDefault();
 
-  const results = document.querySelector('.results pre');
-  results.innerText = JSON.stringify(formJSON, null, 2);
-  FileSystem.writeFile('results.json', results, 'utf8', callback);
+const entry = new FormData(event.target);
+
+const formJSON = Object.fromEntries(entry.entries());
+
+// for multi-selects, we need special handling
+//formJSON.multi = entry.getAll('multi');
+
+const results = document.querySelector('.results pre');
+results.innerText = JSON.stringify(formJSON, null, 2);
+//FileSystem.writeFile('results.json', results, 'utf8', callback);
+
+}});
 }
 
 const form = document.querySelector('.entry-form');
 form.addEventListener('submit', handleFormSubmit);
+
+/*
+function handleFormSubmit(event) {
+
+event.preventDefault();
+
+const data = new FormData(event.target);
+
+const formJSON = Object.fromEntries(data.entries());
+
+// for multi-selects, we need special handling
+//formJSON.multi = data.getAll('multi');
+
+const results = document.querySelector('.results pre');
+results.innerText = JSON.stringify(formJSON, null, 2);
+//FileSystem.writeFile('results.json', results, 'utf8', callback);
+
+}));
+}
+
+const form = document.querySelector('.entry-form');
+form.addEventListener('submit', handleFormSubmit);
+*/
